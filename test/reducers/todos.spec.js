99e7980c1a5407063e1529ee5d21b1f898186970
Michael Reynolds <michael4reynolds@gmail.com>
2017-01-07 13:32:43 -0500

    todos reducer can toggle a todo

--- test/reducers/todos.spec.js
+++ test/reducers/todos.spec.js
@@ -12,12 +12,19 @@ const todos = (state = todosInitialState, action) => {
           completed: false
         }
       ]
+    case 'TOGGLE_TODO':
+      return state.map(t => {
+        return t.id !== action.id ? t : {
+          ...t,
+          completed: !t.completed
+        }
+      })
     default:
       return state
   }
 }
 
-describe('Create', () => {
+describe('Add', () => {
   const stateBefore = []
   const action = {
     type: 'ADD_TODO',
@@ -34,6 +41,38 @@ describe('Create', () => {
   deepFreeze(stateAfter)
 
   it('can add a todo to the list', () => {
+    expect(todos(stateBefore, action)).toEqual(stateAfter)
+  })
+})
+
+describe('Toggle', () => {
+  const stateBefore = [{
+    id: 0,
+    text: 'todo 1',
+    completed: false
+  }, {
+    id: 1,
+    text: 'todo 2',
+    completed: false
+  }]
+  const action = {
+    type: 'TOGGLE_TODO',
+    id: 1
+  }
+  const stateAfter = [{
+    id: 0,
+    text: 'todo 1',
+    completed: false
+  }, {
+    id: 1,
+    text: 'todo 2',
+    completed: true
+  }]
+
+  deepFreeze(stateBefore)
+  deepFreeze(stateAfter)
+
+  it('can toggle a todo', () => {
     expect(todos(stateBefore, action)).toEqual(stateAfter)
   })
 })
