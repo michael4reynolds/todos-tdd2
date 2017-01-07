25739fad1a81a003423b1148f265f82ecb1eaba4
Michael Reynolds <michael4reynolds@gmail.com>
2017-01-07 18:59:43 -0500

    visibility filter reducer can set visibility filter

--- test/reducers/visibility-filter.spec.js
+++ test/reducers/visibility-filter.spec.js
@@ -3,18 +3,32 @@ import deepFreeze from 'deep-freeze'
 const initialState = 'SHOW_ALL'
 export const visibilityFilter = (state = initialState, action) => {
   switch (action.type) {
+    case 'SET_VISIBILITY_FILTER':
+      return action.filter
     default:
       return state
   }
 }
 
 describe('Visibility Filter', () => {
-  const stateBefore = undefined
-  const action = {}
-  const stateAfter = "SHOW_ALL"
-  deepFreeze(action)
-
   it('sets initial state', () => {
+    const stateBefore = undefined
+    const action = {}
+    const stateAfter = "SHOW_ALL"
+    deepFreeze(action)
+
+    expect(visibilityFilter(stateBefore, action)).toEqual(stateAfter)
+  })
+
+  it('can set visibility filter', () => {
+    const stateBefore = 'SHOW_ALL'
+    const action = {
+      type: 'SET_VISIBILITY_FILTER',
+      filter: 'filter'
+    }
+    const stateAfter = 'filter'
+    deepFreeze(stateBefore)
+    deepFreeze(action)
     expect(visibilityFilter(stateBefore, action)).toEqual(stateAfter)
   })
 })
