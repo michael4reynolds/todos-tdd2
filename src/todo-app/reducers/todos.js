67b61fee3eaf5aafe2f667aca81f85d6f8213163
Michael Reynolds <michael4reynolds@gmail.com>
2017-01-07 17:14:39 -0500

    todos reducer uses todo reducer for adding / toggling individual todos

--- src/todo-app/reducers/todos.js
+++ src/todo-app/reducers/todos.js
@@ -1,21 +1,16 @@
+import todo from './todo'
+
 const todosInitialState = []
 const todos = (state = todosInitialState, action) => {
   switch (action.type) {
     case 'ADD_TODO':
       return [
         ...state,
-        {
-          id: action.id,
-          text: action.text,
-          completed: false
-        }
+        todo(undefined, action)
       ]
     case 'TOGGLE_TODO':
       return state.map(t => {
-        return t.id !== action.id ? t : {
-          ...t,
-          completed: !t.completed
-        }
+        return t.id !== action.id ? t : todo(t, action)
       })
     default:
       return state
