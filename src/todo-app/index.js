0ac2595e776cc0d759ecf1173aa13da108887cff
Michael Reynolds <michael4reynolds@gmail.com>
2017-01-08 03:25:30 -0500

    render main app with a button

---
+++ src/todo-app/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {render} from 'react-dom'
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+import appReducer from './reducers/app'
+import TodoApp from './components/app'
+
+const store = createStore(appReducer)
+const app = (
+  <Provider store={store}>
+    <TodoApp/>
+  </Provider>
+)
+render(app, document.getElementById('root'))
