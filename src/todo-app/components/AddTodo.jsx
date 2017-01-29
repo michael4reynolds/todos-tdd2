e8e0922b415df59d299e90500542f5078ab55a73
Michael Reynolds <michael4reynolds@gmail.com>
2017-01-28 21:28:49 -0500

    style header and input form

--- src/todo-app/components/AddTodo.jsx
+++ src/todo-app/components/AddTodo.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import addTodo from '../../action-creators/add-todo'
+import DoubleDown from 'react-icons/lib/fa/angle-double-down'
 
 let AddTodo = ({dispatch}) => {
   let input
   return (
-    <div>
-      <input ref={node => input = node}/>
-      <button onClick={() => {
+    <div className="todo-form">
+      <DoubleDown className="todo-dd"/>
+      <input className="todo-input" ref={node => input = node}/>
+      <button className="input-button" onClick={() => {
         dispatch(addTodo(input.value))
         input.value = ''
       }}>Add Todo
--- src/todo-app/components/app.jsx
+++ src/todo-app/components/app.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import AddTodo from './AddTodo'
 import VisibleTodoList from './TodoList'
 import Footer from './Footer'
+import '../styles/reset.css'
+import '../styles/styles.css'
 
 const TodoApp = () => {
   return (
-    <div>
+    <div className="todo-app">
+      <h1 className="main-heading">Todo Lister</h1>
       <AddTodo/>
       <VisibleTodoList/>
       <Footer/>
